c6167cd6dec9194d147fe721c6b6835d9864cd6b
ncg201064@gmail.com Glascock <ncg201064@gmail.com>
2018-05-02 05:39:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musiciansArray, instrumentsArray) {
   var MusiciansInfo = [];
-  for (let i = 0; i < musiciansArray.length(); i++) {
+  for (let i = 0; i < musiciansArray.length; i++) {
     MusiciansInfo[i] = Object.assign({}, MusiciansInfo, { [i]: `${musiciansArray[i]} plays ${instrumentsArray[i]}`}); 
   }
   return MusiciansInfo;
