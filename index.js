fa496a159bc16aa0c546520ee0343844094c5c94
ncg201064@gmail.com Glascock <ncg201064@gmail.com>
2018-05-02 05:38:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-Function theBeatlesPlay(musiciansArray, )
+function theBeatlesPlay(musiciansArray, instrumentsArray) {
+  var MusiciansInfo = [];
+  for (let i = 0; i < musiciansArray.length(); i++) {
+    MusiciansInfo[i] = Object.assign({}, MusiciansInfo, { [i]: `${musiciansArray[i]} plays ${instrumentsArray[i]}`}); 
+  }
+  return MusiciansInfo;
+}
